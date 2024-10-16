7df107ba57ddf86f89a21ac653fd1dd760259253
saarikoski02 <juusosaarikoski98@gmail.com>
2024-10-16 22:39:44 +0300

    Tuotteiden selaus korjattu

--- scripti.js
+++ scripti.js
@@ -2,15 +2,15 @@ const productContainers = [...document.querySelectorAll('.tuotesäiliö')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
+productContainers.forEach((container, i) => {
+    let containerWidth = container.offsetWidth; // Kontainerin leveys
+    let scrollAmount = containerWidth * 0.9; // Vieritysmäärä yhtä kertaa kohden (90% leveydestä)
 
     nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
+        container.scrollLeft += scrollAmount; // Vieritä oikealle
+    });
 
     preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
+        container.scrollLeft -= scrollAmount; // Vieritä vasemmalle
+    });
+});
